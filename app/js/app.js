60289b1713a3c583038b73be40272b7d220f4af9
konstantinaxt <axtkonstantin@hotmail.de>
2016-07-13 22:24:50 +0200

    add stream  functionality to play tracks

--- app/js/app.js
+++ app/js/app.js
@@ -1,29 +1,43 @@
 import $ from 'jquery';
 import SC from 'soundcloud';
+import SoundCloudAudio from 'soundcloud-audio';
 
-$(document).ready(() => {
-
+$(document).ready(function () {
   const client_id = '4a39a5fc90a81d598101eeaf122056bc';
 
   SC.initialize({
     client_id : client_id
   });
 
-  SC.get('/users/1987006/tracks').then((tracks) => {
-    console.log('tracks: ', tracks);
+  // TODO find a better way to stream soundcloud tracks
+  var scPlayer = new SoundCloudAudio(client_id);
 
+  SC.get('/users/1987006/tracks').then((tracks) => {
+    console.log("tracks: ", tracks);
     const getTrackTemplate = tracks.map((track)=> {
-      const trackUrl = track.stream_url + '?client_id=' + client_id;
       const imgUrl = track.artwork_url === null ? '/img/noImage.png' : track.artwork_url;
-      const imgTag = `<a class="track-icon"><img src="${imgUrl}" /><i class="fa fa-play"></i></a>`;
-      return `<div class="track">${imgTag}<div class="track-info"><h3 class="artist">zeesarOne</h3><span class="track-title">${track.title}</span></div><audio src="${trackUrl}" class="zeesarOneTrack"></audio></div>`;
+      const imgTag = `<a class="track-icon" data-url="${track.id}"><img src="${imgUrl}" /><i class="fa"></i></a>`;
+      return `<div class="track">${imgTag}<div class="track-info"><h3 class="artist">zeesarOne</h3><span class="track-title">${track.title}</span></div></div>`;
     });
 
-    console.log("getTrackTemplate: ", getTrackTemplate);
-
     $('.tracks-wrapper').append(getTrackTemplate);
+
+    const $trackIcon = $('.track-icon');
+    $trackIcon.on('click', function () {
+      const url = $(this).data('url');
+      if (!$(this).hasClass('playing')) {
+        $trackIcon.each(function () {
+          if ($trackIcon.hasClass('playing')) {
+            $trackIcon.removeClass('playing');
+          }
+        });
+        scPlayer.play({streamUrl : `https://api.soundcloud.com/tracks/${url}/stream`});
+        $(this).addClass('playing');
+      } else {
+        scPlayer.stop();
+        $(this).removeClass('playing');
+      }
+    });
   });
-
-
 });
 
